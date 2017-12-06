91365ce4d90cf4d2395132d303103ea7b05d65d5
singhnsatya <singhnsatya@gmail.com>
2017-12-06 20:27:08 +0530

    changes

--- nodejs-postgres-api/routes/index.js
+++ nodejs-postgres-api/routes/index.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
 		})
 	})
 
-	app.post('/users', async (req, res, next) => {
+	app.post('/users', (req, res, next) => {
 		req.check('name', 'Name should not be empty').notEmpty();
 		req.check('age', 'Age must be an number').isDecimal();
 		req.check('gender', 'Gender should not be empty').notEmpty();
@@ -30,10 +30,12 @@ module.exports = (app) => {
 				errors: errors
 			})
 		} else {
-			var users = await queries.createUser(req.body);
-			res.status(200).json({
-				success: true,
-				users: users[0]
+			queries.createUser(req.body)
+			.then(users => {
+				res.status(200).json({
+					success: true,
+					users: users[0]
+				})				
 			})
 		}
 	})
